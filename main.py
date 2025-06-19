eb88ca844b93cab16a879cf4f806c0cc6a1ad8e6
ashahmir <141661326+ashahmir@users.noreply.github.com>
2025-06-19 17:56:12 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the old creek\nReceive Your SuperHero name by answering a few Questions")
+color = input("Enter Your Favourite Color: ")
+bodyPart = input("Enter a random Body Part Name: ")
+print("Your Super Hero Name is: " + color + " " + bodyPart)
